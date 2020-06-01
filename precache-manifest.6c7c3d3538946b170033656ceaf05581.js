4ebe188259b6252985baa1c54df9a59ebd57e057
Chekwa23 <55670771+Chekwa23@users.noreply.github.com>
2020-05-31 22:13:44 -0500

    Updates

---
+++ precache-manifest.6c7c3d3538946b170033656ceaf05581.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "6308e016d873f106157d",
+    "url": "/AboutTime/static/css/main.672ea331.chunk.css"
+  },
+  {
+    "revision": "6308e016d873f106157d",
+    "url": "/AboutTime/static/js/main.13cbc257.chunk.js"
+  },
+  {
+    "revision": "36e1d19548e4db221a69",
+    "url": "/AboutTime/static/js/runtime~main.b288673c.js"
+  },
+  {
+    "revision": "c5d56ae2924cf6513211",
+    "url": "/AboutTime/static/css/2.037a2267.chunk.css"
+  },
+  {
+    "revision": "c5d56ae2924cf6513211",
+    "url": "/AboutTime/static/js/2.2712f646.chunk.js"
+  },
+  {
+    "revision": "edecde609103ccfbe8744c3ebd9ef3f5",
+    "url": "/AboutTime/static/media/iphoneIcon.edecde60.svg"
+  },
+  {
+    "revision": "2706f8f77766113db7c270364a186140",
+    "url": "/AboutTime/static/media/yellowdotsIcon.2706f8f7.svg"
+  },
+  {
+    "revision": "e3aa86c95e9258bd3a934114edef49d3",
+    "url": "/AboutTime/static/media/restaurantIcon.e3aa86c9.svg"
+  },
+  {
+    "revision": "3492f89f338507dd72c2f0e32fb1b35c",
+    "url": "/AboutTime/static/media/appLogo.3492f89f.svg"
+  },
+  {
+    "revision": "e56064a821ba68de812378bb697686d7",
+    "url": "/AboutTime/static/media/playstoreIcon.e56064a8.svg"
+  },
+  {
+    "revision": "be328bd8e8025f7a01ce17451425596e",
+    "url": "/AboutTime/static/media/appstoreIcon.be328bd8.svg"
+  },
+  {
+    "revision": "e1992b63bcab2e9dd4e810c109944501",
+    "url": "/AboutTime/static/media/locationLogo.e1992b63.svg"
+  },
+  {
+    "revision": "ea4e5df77edad5b8a8e92b4c4d84e771",
+    "url": "/AboutTime/static/media/roadLogo.ea4e5df7.svg"
+  },
+  {
+    "revision": "c5eef9ee2fbd4df584274dd25b13cbef",
+    "url": "/AboutTime/static/media/barLogo.c5eef9ee.svg"
+  },
+  {
+    "revision": "fbf92611ec57d473b98a436c2e67cd95",
+    "url": "/AboutTime/static/media/redlineIcon.fbf92611.svg"
+  },
+  {
+    "revision": "14d261430260ff82917e5a684c948092",
+    "url": "/AboutTime/static/media/polygonLogo.14d26143.svg"
+  },
+  {
+    "revision": "77686e70e55a4160424d85f20d42fb8c",
+    "url": "/AboutTime/index.html"
+  }
+];
