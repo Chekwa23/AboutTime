d5fe5c69ad83c0c65a4547e0e2c221acaae6d00d
Chekwa23 <55670771+Chekwa23@users.noreply.github.com>
2020-05-31 22:05:21 -0500

    refurbished

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import LandingPage from './LandingPage';
+import signInPage from './signInPage';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <LandingPage />
+    {/* <signInPage /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
